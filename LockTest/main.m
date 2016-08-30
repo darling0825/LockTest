49deb2b52b2bc292ffa875099bee07bcc9c3a5e8
darling0825 <darling0825@163.com>
2016-08-30 10:33:51 +0800

    init

---
+++ LockTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LockTest
+//
+//  Created by 沧海无际 on 16/8/30.
+//  Copyright © 2016年 darling0825. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
